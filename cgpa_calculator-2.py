251bfa4146b430cd43f8ba087bf409b9a0209872
Y.RAJASEKHAR <sekharyemineni3@gmail.com>
2025-06-08 19:17:36 +0530

    Update cgpa_calculator-2.py
    made small changes in input handling

--- cgpa_calculator-2.py
+++ cgpa_calculator-2.py
@@ -12,7 +12,7 @@ def get_valid_grade(prompt):
 
 class cgpa_calculator:
     def disclaimer(self):
-        print("\n*THIS CODE FOLLOWS GENERAL CGPA CALCULATION FORMULA OF JNTUK R19 REGULATION*\n")
+        print("\n**THIS CODE FOLLOWS GENERAL CGPA CALCULATION FORMULA OF JNTUK R19 REGULATION**\n")
     def __init__(self):
         self.subject_total=0
         self.labs_total=0
@@ -24,36 +24,36 @@ class cgpa_calculator:
 
     def subjects(self):
         print("---------------------------------------------------------------------------------")
-        subject=int(input("Enter number of subjects: "))
+        subject=int(input("Enter number of subjects : "))
         for i in range(subject):
-            grade= get_valid_grade("Enter grade of subject "+ str(i+1) +":")
+            grade= get_valid_grade("Enter grade of subject "+ str(i+1) +" : ")
             self.subject_total+=grade*3
             self.total_credits+=3
 
     def labs(self):
         print("---------------------------------------------------------------------------------")
-        lab=int(input("Enter number of labs: "))
+        lab=int(input("Enter number of labs : "))
         for i in range(lab):
-            grade=get_valid_grade("Enter grade of lab "+ str(i+1) +":")
+            grade=get_valid_grade("Enter grade of lab "+ str(i+1) +" : ")
             self.labs_total+=grade*1.5
             self.total_credits+=1.5
 
     def project(self):
         print("---------------------------------------------------------------------------------")
-        grade=get_valid_grade("Enter grade of project: ")
+        grade=get_valid_grade("Enter grade of project : ")
         self.projects=grade*9
         self.total_credits+=9
 
     def other_subjects(self):
         print("---------------------------------------------------------------------------------")
-        grade=get_valid_grade("Enter grade of Engineering drawing: ")
+        grade=get_valid_grade("Enter grade of Engineering drawing : ")
         self.drawing+=grade*2.5
         self.total_credits+=2.5
         print("---------------------------------------------------------------------------------")
 
-        other_sub = int(input("Enter number of subjects that have 1 credit :"))
+        other_sub = int(input("Enter number of subjects that have 1 credit : "))
         for i in range(other_sub):
-            grade=get_valid_grade("Enter grade of 1-credit subject "+ str(i+1) +":")
+            grade=get_valid_grade("Enter grade of 1-credit subject "+ str(i+1) +" : ")
             self.other_subject+=grade*1
             self.total_credits+=1
 
